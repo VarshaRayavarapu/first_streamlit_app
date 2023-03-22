cfac037efe40e5d7ed4ac1e26bc9571aa042ef7f
Varsha Rayavarapu <116050720+VarshaRayavarapu@users.noreply.github.com>
2023-03-22 16:17:29 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
-import streamlit
+import Streamlit
 
-streamlit.title('My Mom's New Healthy Diner')
+Streamlit.title('My Mom's New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
-streamlit.text(' 🥑🍞 Avacado Toast')
+Streamlit.header('Breakfast Favourites')
+Streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
+Streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+Streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+Streamlit.text(' 🥑🍞 Avacado Toast')

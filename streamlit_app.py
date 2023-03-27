1b4b5242fcc2af1740aed8bcb5e5aa4d377a9972
Varsha Rayavarapu <116050720+VarshaRayavarapu@users.noreply.github.com>
2023-03-27 11:47:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,51 +1,37 @@
 import streamlit
-
 streamlit.title("My Mom\'s New Healthy Diner")
-
 streamlit.header('Breakfast Favourites')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avacado Toast')
-
 streamlit.header(" 🍌 🥭 Build Your Own Fruit Smoothie 🥝 🍇 ")
-
 import pandas
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 # Display the table on the page.
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 streamlit.dataframe(my_fruit_list)
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=["Avocado", "Strawberries"]
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),fruits_selected)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 ##Display the table on the page 
 streamlit.dataframe(fruits_to_show)
-
 ## New Section to Display api Response
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-
 ## New Section to Display Fruityvice Api Response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 ##streamlit.text(fruityvice_response.json()) ## just write the data to the screen
-
 ## take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-
 ##output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
-
 import snowflake.connector
-
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-##my_cur = my_cnx.cursor()
+my_cur = my_cnx.cursor()
 ##my_cur.execute("SELECT CURRENT_USER(varsharayavarapu), CURRENT_ACCOUNT(yj21681)")
 ##my_data_row = my_cur.fetchone()
 ##streamlit.text("Hello from Snowflake:")

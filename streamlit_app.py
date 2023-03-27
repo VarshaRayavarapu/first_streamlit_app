b5f1e0e37644d2053e2ee7994429ec9669c82abf
Varsha Rayavarapu <116050720+VarshaRayavarapu@users.noreply.github.com>
2023-03-27 12:15:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,12 +33,10 @@ import snowflake.connector
 @streamlit.experimental_singleton
 def init_connection():
     return snowflake.connector.connect(**streamlit.secrets["snowflake"])
-conn = init_connection()
-  
-  
-##(my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-##my_cur = my_cnx.cursor()
-##my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-##my_data_row = my_cur.fetchone()
-##streamlit.text("Hello from Snowflake:")
-##streamlit.text(my_data_row)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)

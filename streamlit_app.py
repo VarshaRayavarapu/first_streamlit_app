0f6ecaf10a4b6d4a575b146f202334385dee2494
Varsha Rayavarapu <116050720+VarshaRayavarapu@users.noreply.github.com>
2023-03-27 14:13:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
-
+import pandas
+import requests
+import snowflake.connector
+from urllib.error import URLError
 streamlit.title("My Mom\'s New Healthy Diner")
 streamlit.header('Breakfast Favourites')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
@@ -7,7 +10,7 @@ streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞 Avacado Toast')
 streamlit.header(" 🍌 🥭 Build Your Own Fruit Smoothie 🥝 🍇 ")
-import pandas
+
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 # Display the table on the page.
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -24,13 +27,16 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 ## New Section to Display Fruityvice Api Response
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 ##streamlit.text(fruityvice_response.json()) ## just write the data to the screen
 ## take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 ##output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+#don't un anything past here while wetroublehoot
+streamlit.stop()
 
 import snowflake.connector
 @streamlit.experimental_singleton

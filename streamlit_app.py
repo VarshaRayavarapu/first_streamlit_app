aad7731b318b17798ab0a1a96a02ef2c285622c4
Varsha Rayavarapu <116050720+VarshaRayavarapu@users.noreply.github.com>
2023-03-27 11:29:11 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,15 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 ##streamlit.text(fruityvice_response.json()) ## just write the data to the screen
 
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(varsharayavarapu), CURRENT_ACCOUNT(yj21681), CURRENT_REGION(US-EAST-2.aws)")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 
 ## take the json version of the response and normalize it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
